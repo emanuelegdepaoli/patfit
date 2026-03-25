82fc5181c99a0ed3c247ab0c2720cdb6bf6544a7
emanuelegdepaoli <emanuelegiovanni.depaoli@gmail.com>
2026-03-25 19:38:07 +0100

    aggiunte domande consulenti e distributori; aggiunta sidebar

--- app.py
+++ app.py
@@ -2,23 +2,66 @@ import streamlit as st
 import random
 import csv
 from collections import defaultdict
+import os
 
-CSV_PATH = "domande.csv"
+# cartella domande
+BASE_PATH = "./domande"
 
-# domande da 1,2,3 punti
-# 20 domande estratte in modo tale che il totale dei punti sia 40
+FILES = {
+    "Utilizzatori": "utilizzatori.csv",
+    "Distributori": "distributori.csv",
+    "Consulenti": "consulenti.csv",
+}
+
 PASS_SCORE = 32
+
+
+# -------------------------
+# SIDEBAR
+# -------------------------
+
+st.set_page_config(page_title="Simulatore Patentino", layout="wide")
+
+st.sidebar.title("📂 Selezione categoria")
+
+categoria = st.sidebar.radio(
+    "Esame per:",
+    list(FILES.keys())
+)
+
+CSV_PATH = os.path.join(BASE_PATH, FILES[categoria])
+
+# spazio flessibile per spingere in basso
+st.sidebar.markdown("<br><br><br><br><br><br><br><br>", unsafe_allow_html=True)
+
+st.sidebar.markdown("---")
+
+st.sidebar.markdown("Questo progetto è gratuito ed è sviluppato nel tempo libero. Se ti ha aiutato a studiare, puoi supportarlo con un piccolo contributo. Grazie!")
+
+st.sidebar.markdown(
+    """
+    <a href="https://www.buymeacoffee.com/momodepa" target="_blank">
+        <img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" 
+             alt="Buy Me A Coffee" 
+    </a>
+    """,
+    unsafe_allow_html=True
+)
+
+st.sidebar.markdown("---")
+
+st.sidebar.markdown("📩 Hai trovato un errore o hai dei suggerimenti per migliorarlo? [Contattami](mailto:emanueledepaoli1@gmail.com)")
 
 # -------------------------
 # CARICAMENTO DOMANDE
 # -------------------------
 
 @st.cache_data
-def leggi_domande():
+def leggi_domande(csv_path):
 
     domande = defaultdict(list)
 
-    with open(CSV_PATH, newline="", encoding="utf-8") as csvfile:
+    with open(csv_path, newline="", encoding="utf-8") as csvfile:
 
         reader = csv.DictReader(csvfile, delimiter=";")
 
@@ -50,7 +93,7 @@ def genera_esame(domande):
 
     n1 = random.randint(0, 10)
     n3 = n1
-    n2 = 20 - 2 * n3
+    n2 = 20 - 2 * n1
 
     estratte = (
         random.sample(domande[1], n1)
@@ -97,28 +140,36 @@ def correggi_esame(esame, risposte):
 # UI
 # -------------------------
 
-st.set_page_config(page_title="Simulatore Patentino", layout="wide")
+st.title("🌱 Simulatore Esame Patentino Fitosanitario")
+st.subheader(f"Quesiti per {categoria}")
 
-st.title("🌱 Simulatore Esame Regione Piemonte per Patentino Fitosanitario")
-st.subheader("Quesiti per utilizzatori")
+domande = leggi_domande(CSV_PATH)
 
-domande = leggi_domande()
+# -------------------------
+# STATE
+# -------------------------
 
+if "esame" not in st.session_state:
+    st.session_state.esame = None
+    st.session_state.risposte = {}
+
+# reset automatico quando cambio categoria
+if "categoria_corrente" not in st.session_state:
+    st.session_state.categoria_corrente = categoria
+
+if st.session_state.categoria_corrente != categoria:
+    st.session_state.esame = None
+    st.session_state.risposte = {}
+    st.session_state.categoria_corrente = categoria
 
 # -------------------------
 # GENERA ESAME
 # -------------------------
 
-# inizializzazione state
-if "esame" not in st.session_state:
-    st.session_state.esame = None
-    st.session_state.risposte = {}
-
 if st.button("🎯 Genera nuovo esame"):
 
     st.session_state.esame = genera_esame(domande)
     st.session_state.risposte = {}
-
 
 # -------------------------
 # MOSTRA ESAME
@@ -144,7 +195,6 @@ if st.session_state.esame:
 
         st.divider()
 
-
     # -------------------------
     # CONSEGNA
     # -------------------------
@@ -167,7 +217,6 @@ if st.session_state.esame:
 
         st.progress(punteggio / 40)
 
-
         if errori:
 
             st.subheader("Risposte sbagliate")
@@ -175,15 +224,8 @@ if st.session_state.esame:
             for e in errori:
 
                 st.write("**Domanda:**", e["domanda"])
-
-                st.write(
-                    f"Tua risposta: {e['tua_risposta']}"
-                )
-
-                st.write(
-                    f"Corretta: {e['corretta_risposta']}"
-                )
-
+                st.write(f"Tua risposta: {e['tua_risposta']}")
+                st.write(f"Corretta: {e['corretta_risposta']}")
                 st.write(f"Punti domanda: {e['pti']}")
 
                 st.divider()
